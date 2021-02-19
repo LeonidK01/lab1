880610b3adf7be63ac2ead55a35ef6e408afefda
Leonid <en.lenya@mail.ru>
2021-02-19 21:53:46 +0300

    Задание 4
    Факториал рекурсией вниз

--- lab3.pl
+++ lab3.pl
@@ -5,3 +5,6 @@ max(_,Y,U,Y):-Y>U,!.
 max(_,_,U,U):-!.
 fact(1,1):-!.
 fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
+fact(1,Cur_x,Cur_x):-!.
+fact(N,Cur_x,X):-Cur_x1 is Cur_x*N, N1 is N-1, fact(N1,Cur_x1,X).
+factorial(N,X):-fact(N,1,X).
