d37a2fa2bf2d666f55b8dbbe204ea189b2f1ae9a
Leonid <en.lenya@mail.ru>
2021-03-21 13:54:51 +0300

    Задания 2
    Предикат суммы рекурсией вниз

--- lab4.pl
+++ lab4.pl
@@ -4,3 +4,8 @@ read_list(0,[]):-!.
 read_list(N,X):-write("Введите элемент"),nl,read(X1), N1 is N-1,read_list(N1,X2),append1([X1],X2,X).
 write_list([X]):-write(X).
 write_list([X|Y]):-write(X),write_list(Y).
+
+sum_list_downn(N,Sum):-read_list(N,X),sum_list_down(X,Sum).
+sum_list_down(List,Sum):-sum_list_down(List,0,Sum).
+sum_list_down([],Sum,Sum):-!.
+sum_list_down([Head|Tail],S,Sum):-S1 is S+Head,sum_list_down(Tail,S1,Sum).
