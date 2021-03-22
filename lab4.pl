591f5dd53ca88ec05a5765c0505c78a3c88e2847
Leonid <en.lenya@mail.ru>
2021-03-22 11:28:59 +0300

    Задание 18,37

--- lab4.pl
+++ lab4.pl
@@ -79,3 +79,8 @@ min_list_down1([H|T],Temp,Min,Elem):-H<Temp,not(H==Elem),min_list_down1(T,H,Min,
 min_list_down1([_|T],Temp,Min,Elem):-min_list_down1(T,Temp,Min,Elem).
 
 min_elem(List,Kol):-min_list_down(List,Elem),kol(List,Elem,Kol).
+
+sosed_left(List,Kol):-sosed_left(List,0,0,Kol).
+sosed_left([],_,Kol,Kol):-!.
+sosed_left([A,B|T],Cout, Kolvo,Kol):-A>B,Count1 is Cout+1,Kolvo1 is Kolvo + 1,write(Count1),sosed_left(T,Count1,Kolvo1,Kol),!.
+sosed_left([_|T],Cout,Kolvo,Kol):-Cout1 is Cout+1,sosed_left(T,Cout1,Kolvo,Kol),!.
