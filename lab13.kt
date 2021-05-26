443b608b42ddec5cc2f90d6a09bc840e8493e67a
Leonid <en.lenya@mail.ru>
2021-05-26 21:51:17 +0300

    Задание 7

--- lab13.kt
+++ lab13.kt
@@ -35,3 +35,40 @@ fun main()
     }
     println(sum)
 }
+fun sumNumber(x: Int) : Int
+{
+    var x_met = x
+    var sum = 0
+    while (x_met != 0)
+    {
+        sum += x_met % 10
+        x_met /= 10
+    }
+    return sum
+}
+fun proiz(x: Int) : Int
+{
+    var n = x
+    var proiz = 1
+    while (n != 0)
+    {
+        proiz *= n % 10
+        n /= 10
+    }
+    return proiz
+}
+
+fun max(num:Int): Int = if (num < 10) num else if (num % 10 > max(num/10)) num % 10 else max(num/10)
+
+fun min(num:Int): Int = if (num < 10) num else if (num % 10 < min(num/10)) num % 10 else min(num/10)
+
+fun main() {
+    println("Введите число")
+    val x: Int = readLine()!!.toInt()
+    val max: Int = max(x)
+    val min: Int = min(x)
+    val proi : Int=proiz(x)
+    println("max = $max")
+    println("min = $min")
+    println("Произведение = $proi")
+}
