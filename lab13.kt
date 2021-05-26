f07315d637c8d3c6c749a5a7b7c92be2ddc5c255
Leonid <en.lenya@mail.ru>
2021-05-26 20:46:48 +0300

    Задание 5

--- lab13.kt
+++ lab13.kt
@@ -5,3 +5,21 @@ fun main() {
     val  name = readLine()
     println("Здравствуйте, $name!")
 }
+fun getLanguage(a:String, b:String) =
+    when{
+        a=="Prolog" -> println("$b,Вы - подлиза :Р")
+        a=="Kotlin" -> println("$b, Вы - подлиза :Р")
+        a=="c++" -> println("$b, выбор хороший конечно, но не в этом случае сишник")
+        a=="с#" -> println("$b, выбор хороший конечно, но не в этом случае сишник")
+        a=="ruby" -> println("$b, выбор хороший конечно, тут это оценят")
+        a=="java" -> println("$b, выбор хороший конечно, и похож на этот, ты дждимшик")
+        else -> println("$b я не знаю такого, поэтому ты просто минипрограммист")
+    }
+fun main() {
+
+    println("Как тебя зовут?")
+    val  name = readLine()
+    println("Привет, $name! Какой твой любимый язык программирования?")
+    val favlan = readLine()
+    getLanguage(favlan.toString(), name.toString())
+}
